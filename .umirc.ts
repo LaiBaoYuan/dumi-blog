ad695c6cabcdd09045eebbf4010ef8628b0652b5
lby <1727283040@qq.com>
2022-10-14 07:18:03 +0800

    init

---
+++ .umirc.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'dumi';
+
+export default defineConfig({
+  title: '程序员阿元',
+  favicon:
+    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  outputPath: 'docs-dist',
+  mode: 'site',
+  navs: [
+    null, // null 值代表保留约定式生成的导航，只做增量配置
+    {
+      title: 'GitHub',
+      path: 'https://github.com/umijs/dumi',
+    }
+  ],
+});
---
+++ docs/index.md
@@ -0,0 +1,20 @@
+---
+hero:
+  title: 程序员阿元
+  desc: 自学前端转码之路
+  actions:
+    - text: 快上车
+      link: /components
+footer: Open-source MIT Licensed | Copyright © 2022<br />Powered by [dumi](https://d.umijs.org)
+---
+
+## Hello umi-com!
+
+Demo:
+
+```tsx
+import React from 'react';
+import { Foo } from 'umi-com';
+
+export default () => <Foo title="First Demo" />;
+```
